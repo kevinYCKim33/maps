9482af98b20f8abd93dbf1121d587e1a62c5016c
Kevin Y. Kim <kevinyckim33@gmail.com>
2021-02-08 22:45:04 -0500

    Finish 56 of 279 export statements in typescript

--- src/User.ts
+++ src/User.ts
@@ -1,3 +1,22 @@
-class User {
-    
+// parcel index.html
+import faker from "faker";
+
+// I guess TS doesn't really do export default
+// generally a no no in TS community
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    // this.location.lat = parseFloat(faker.address.latitude()) // this surprisingly doesn't work
+    // because location is technically undefined?? huh???
+    this.location = {
+      lat: parseFloat(faker.address.latitude()), // faker library has lat, lng as str, which Grider says is oversight
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }
